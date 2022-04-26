9b8f14049e045567d5736bc3973013dcd5556b91
David Souza <dabcs@poli.ufrj.br>
2022-04-26 15:17:51 -0300

    correção checkstyle

--- src/main/java/com/trybe/acc/java/AntecessorSucessor.java
+++ src/main/java/com/trybe/acc/java/AntecessorSucessor.java
@@ -4,14 +4,14 @@ import java.util.Scanner;
 
 public class AntecessorSucessor {
 
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Digite um número:");
-		int numero = scan.nextInt();
-		int antecessor = numero - 1;
-		int sucessor = numero + 1;
-		System.out.println("O número " + numero + " tem "+ antecessor + " como seu antecessor e " + sucessor + " como seu sucessor.");
+public static void main(String[] args) {
+	Scanner scan = new Scanner(System.in);
+	System.out.println("Digite um número:");
+	int numero = scan.nextInt();
+	int antecessor = numero - 1;
+	int sucessor = numero + 1;
+	System.out.println("O número " + numero + " tem "+ antecessor + " como seu antecessor e " + sucessor + " como seu sucessor.");
 
-	}
+}
 
 }
