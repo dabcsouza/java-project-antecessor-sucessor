7a96c97d0f460c0d20834a1b957885f3d01099a6
David Souza <dabcs@poli.ufrj.br>
2022-04-26 15:12:35 -0300

    exercicio 1

--- src/main/java/com/trybe/acc/java/AntecessorSucessor.java
+++ src/main/java/com/trybe/acc/java/AntecessorSucessor.java
@@ -5,16 +5,12 @@ import java.util.Scanner;
 public class AntecessorSucessor {
 
 	public static void main(String[] args) {
-		// Ler a entrada de um número pelo console	
 		Scanner scan = new Scanner(System.in);
-
 		System.out.println("Digite um número:");
 		int numero = scan.nextInt();
-		
-		// Seu código aqui
-				
-		// Imprime a saída no console. OBS: descomente a próxima linha
-		//System.out.println("O número " + numero + " tem "+ antecessor + " como seu antecessor e " + sucessor + " como seu sucessor.");
+		int antecessor = numero - 1;
+		int sucessor = numero + 1;
+		System.out.println("O número " + numero + " tem "+ antecessor + " como seu antecessor e " + sucessor + " como seu sucessor.");
 
 	}
 
